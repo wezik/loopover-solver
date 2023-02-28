2671e18ec71e755d729b54a57007c813b8255ae3
Dominik Kondrot <dominik.kondrot@gmail.com>
2023-02-28 15:37:58 +0100

    Added mapper unit tests

--- src/loopover/mapper.rs
+++ src/loopover/mapper.rs
@@ -44,3 +44,80 @@ fn valid_boards(current_board: &Board, solved_board: &Board) -> bool {
         && current_board.get_height() == solved_board.get_height();
     are_tiles_same && are_boards_equal
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::loopover::mapper::valid_boards;
+    use crate::loopover::structs::{Board, Tile};
+
+    fn create_test_board(values: Vec<&str>, width: usize, height: usize) -> Board {
+        generate_board(values, width, height).expect("incorrect input on test board")
+    }
+
+    fn generate_board(values: Vec<&str>, width: usize, height: usize) -> Option<Board> {
+        if values.len() != width * height {
+            return None;
+        }
+        let mut tiles: Vec<Vec<Tile>> = Vec::new();
+        for y in 0..height {
+            let mut row = Vec::new();
+            for x in 0..width {
+                row.push(Tile::new(values[x + (height - 1) * y].to_string()));
+            }
+            tiles.push(row);
+        }
+        Some(Board::new(tiles).expect("Failed creating board"))
+    }
+
+    #[test]
+    fn test_boards_validation_when_same_diff_dimensions() {
+        // Given
+        let board1 = create_test_board(vec!["a", "b", "c", "d", "e", "f"], 2, 3);
+        let board2 = create_test_board(vec!["a", "b", "c", "d", "e", "f"], 3, 2);
+
+        // When
+        let result = valid_boards(&board1, &board2);
+
+        // Then
+        assert!(!result);
+    }
+
+    #[test]
+    fn test_boards_validation_when_diff_size() {
+        // Given
+        let board1 = create_test_board(vec!["a", "b", "c", "d", "e", "f"], 2, 3);
+        let board2 = create_test_board(vec!["a", "b", "c", "d", "e", "f", "g", "h", "i"], 3, 3);
+
+        // When
+        let result = valid_boards(&board1, &board2);
+
+        // Then
+        assert!(!result);
+    }
+
+    #[test]
+    fn test_boards_validation_when_diff_values() {
+        // Given
+        let board1 = create_test_board(vec!["a", "b", "c", "d", "e", "f"], 2, 3);
+        let board2 = create_test_board(vec!["a", "b", "c", "g", "e", "f"], 2, 3);
+
+        // When
+        let result = valid_boards(&board1, &board2);
+
+        // Then
+        assert!(!result);
+    }
+
+    #[test]
+    fn test_boards_validation_when_same() {
+        // Given
+        let board1 = create_test_board(vec!["a", "b", "c", "d", "e", "f", "g", "h", "i"], 3, 3);
+        let board2 = create_test_board(vec!["a", "b", "c", "d", "e", "f", "g", "h", "i"], 3, 3);
+
+        // When
+        let result = valid_boards(&board1, &board2);
+
+        // Then
+        assert!(result);
+    }
+}
