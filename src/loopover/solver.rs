9238cd7f69e212ad61c9a5139b2931b9955aae41
Dominik Kondrot <dominik.kondrot@gmail.com>
2023-03-04 17:19:50 +0100

    Renaming

--- src/loopover/solver.rs
+++ src/loopover/solver.rs
@@ -3,14 +3,14 @@ use crate::loopover::structs::Board;
 mod instructions;
 mod movement;
 
-pub fn solve(board: &mut Board, solved_board: &Board) -> String {
-    execute_instructions(board, solved_board);
-    format!("{:?}", board.get_moves())
+pub fn solve(unsolved_board: &mut Board, solved_board: &Board) -> String {
+    execute_instructions(unsolved_board, solved_board);
+    format!("{:?}", unsolved_board.get_moves())
 }
 
-fn execute_instructions(board: &mut Board, solved_board: &Board) {
-    instructions::solve_last_row(board, solved_board);
-    for y in 0..board.get_height() - 2 {
-        instructions::solve_row(board, solved_board, y);
+fn execute_instructions(unsolved_board: &mut Board, solved_board: &Board) {
+    instructions::solve_bottom_row(unsolved_board, solved_board);
+    for y in 0..unsolved_board.get_height() - 2 {
+        instructions::solve_board_row(unsolved_board, solved_board, y);
     }
 }
--- src/loopover/solver/instructions.rs
+++ src/loopover/solver/instructions.rs
@@ -2,83 +2,83 @@ use crate::loopover::solver::movement;
 use crate::loopover::structs::Board;
 use crate::ErrorMessage;
 
-pub fn solve_last_row(board: &mut Board, solved_board: &Board) {
-    for x in 0..board.get_width() {
-        let (mut pos_x, mut pos_y) = board
-            .find_tile(solved_board.get_tile(x, board.get_height() - 1))
+pub fn solve_bottom_row(unsolved_board: &mut Board, solved_board: &Board) {
+    for x in 0..unsolved_board.get_width() {
+        let (mut pos_x, mut pos_y) = unsolved_board
+            .find_tile(solved_board.get_tile(x, unsolved_board.get_height() - 1))
             .unwrap_or_else(|| panic!("{}", ErrorMessage::BoardsMismatched.get_message()));
 
-        if pos_x != x && pos_y == board.get_height() - 1 {
-            (pos_x, pos_y) = movement::move_col(board, pos_x, pos_y, -1);
+        if pos_x != x && pos_y == unsolved_board.get_height() - 1 {
+            (pos_x, pos_y) = movement::move_col(unsolved_board, pos_x, pos_y, -1);
         }
 
-        (pos_x, pos_y) = movement::move_row(board, pos_x, pos_y, calculate_offset(pos_x, x));
+        (pos_x, pos_y) = movement::move_row(unsolved_board, pos_x, pos_y, calculate_offset(pos_x, x));
 
         movement::move_col(
-            board,
+            unsolved_board,
             pos_x,
             pos_y,
-            calculate_offset(pos_y, board.get_height() - 1),
+            calculate_offset(pos_y, unsolved_board.get_height() - 1),
         );
     }
 }
 
-pub fn solve_row(board: &mut Board, solved_board: &Board, y: usize) {
-    for x in 0..board.get_width() {
-        let (mut pos_x, mut pos_y) = board
+pub fn solve_board_row(unsolved_board: &mut Board, solved_board: &Board, y: usize) {
+    for x in 0..unsolved_board.get_width() {
+        let (mut pos_x, mut pos_y) = unsolved_board
             .find_tile(solved_board.get_tile(x, y))
             .unwrap_or_else(|| panic!("{}", ErrorMessage::BoardsMismatched.get_message()));
 
         let mut helper_col_used = false;
         let first = x == 0;
-        let last = x == board.get_width() - 1;
+        let last = x == unsolved_board.get_width() - 1;
         let mut helper_col_x = 0;
 
-        if !first && pos_y == y && pos_x != board.get_width() - 1 {
+        if !first && pos_y == y && pos_x != unsolved_board.get_width() - 1 {
             // Move tile col down
-            (pos_x, pos_y) = movement::move_col(board, pos_x, pos_y, 1);
+            (pos_x, pos_y) = movement::move_col(unsolved_board, pos_x, pos_y, 1);
             helper_col_used = true;
             helper_col_x = pos_x;
         }
 
         if last {
             if pos_x == x {
-                (pos_x, pos_y) = movement::move_row(board, pos_x, pos_y, -1);
+                (pos_x, pos_y) = movement::move_row(unsolved_board, pos_x, pos_y, -1);
             }
-            movement::move_col(board, board.get_width() - 1, y, calculate_offset(y, pos_y));
+            movement::move_col(unsolved_board, unsolved_board.get_width() - 1, y, calculate_offset(y, pos_y));
         }
 
         if !(first && pos_y == y) {
             // Move to elevator
             (pos_x, pos_y) = movement::move_row(
-                board,
+                unsolved_board,
                 pos_x,
                 pos_y,
-                calculate_offset(pos_x, board.get_width() - 1),
+                calculate_offset(pos_x, unsolved_board.get_width() - 1),
             );
         }
 
         // Move old tile col up IF HAD TO BEFORE
         if helper_col_used {
-            movement::move_col(board, helper_col_x, 0, -1);
+            movement::move_col(unsolved_board, helper_col_x, 0, -1);
         }
 
         let mut elevator_offset = pos_y as isize;
 
         // Elevator up
-        (pos_x, pos_y) = movement::move_col(board, pos_x, pos_y, calculate_offset(pos_y, y));
+        (pos_x, pos_y) = movement::move_col(unsolved_board, pos_x, pos_y, calculate_offset(pos_y, y));
 
         if !last {
             elevator_offset -= pos_y as isize;
             // To Spot In Line
             movement::move_row(
-                board,
+                unsolved_board,
                 pos_x,
                 pos_y,
-                calculate_offset(pos_x, board.get_width() - 2),
+                calculate_offset(pos_x, unsolved_board.get_width() - 2),
             );
             // Elevator back down
-            movement::move_col(board, board.get_width() - 1, 0, elevator_offset);
+            movement::move_col(unsolved_board, unsolved_board.get_width() - 1, 0, elevator_offset);
         }
     }
 }
